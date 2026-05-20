d02bd5a75462f3faeecf3aed780ac4abeb74a2ae
Rowthu Naveen Kumar <naveenkumar910016@gmail.com>
2026-05-20 15:23:03 +0530

    inital backend setup

---
+++ backend/ai-startup-platform/src/main/java/com/team/ai_startup_platform/AiStartupPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.team.ai_startup_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiStartupPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiStartupPlatformApplication.class, args);
+	}
+
+}
---
+++ backend/ai-startup-platform/src/test/java/com/team/ai_startup_platform/AiStartupPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team.ai_startup_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiStartupPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
